ff85cf6b4bb7053dccdb52e6ad718f244305c21e
Emil Nielsen <342703@RTS-365.dk>
2019-06-12 13:27:07 +0200

    rettet mysql.js så der kan hentes ting fra db via routes; generere menu via categorier hentet fra db; fjernet alt overflødigt indhold på testside for at få bedre overblik over hvad der bliver smidt ind via js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,3 +1,6 @@
+const mysql = require("../config/mysql");
+const date = require("date-and-time");
+
 module.exports = (app) => {
 
    app.get('/', (req, res, next) => {
@@ -246,111 +249,32 @@ module.exports = (app) => {
    });
    
    // Used to try different ways of putting the different pages together
-   app.get('/test', (req, res, next) => {
+   app.get('/test', async (req, res, next) => {
 
-      let categories = [
-         {
-            "categoryID": 1,
-            "categoryName": "politics"
-         },
-         {
-            "categoryID": 2,
-            "categoryName": "breaking news"
-         },
-         {
-            "categoryID": 3,
-            "categoryName": "business"
-         },
-         {
-            "categoryID": 4,
-            "categoryName": "technology"
-         },
-         {
-            "categoryID": 5,
-            "categoryName": "health"
-         },
-         {
-            "categoryID": 6,
-            "categoryName": "travel"
-         },
-         {
-            "categoryID": 7,
-            "categoryName": "sports"
-         },
-      ];
-
-      let latestComments = [
-         {
-            "name": "Comment the 1st",
-            "date": "2019-04-14 7:45:00"
-         },
-         {
-            "name": "2nd Comment",
-            "date": "2019-04-14 9:10:00"
-         },
-         {
-            "name": "a 3rd Comment",
-            "date": "2019-04-14 8:05:00"
-         },
-         {
-            "name": "May the 4th be with me",
-            "date": "2019-04-14 7:25:00"
-         },
-         {
-            "name": "Comment the 5th?",
-            "date": "2019-04-14 6:58:00"
-         },
-         {
-            "name": "End of the line buddy... 6th comment",
-            "date": "2019-04-13 7:35:00"
-         },
-      ];
+      let db = await mysql.connect();
+      let [categories] = await db.execute("SELECT * FROM categories");
+      // let [articles] = await db.execute("SELECT * FROM articles WHERE fk_category_id = ?", [req.params.test_id]);
+      db.end();
 
       res.render('test', {
          "title": "The Amazing Test Page",
-         "latestComments": latestComments,
-         "categories": categories
+         "categories": categories/* ,
+         "articles": articles */
       });
    });
 
    // Now testing with urlParams
    app.get('/test/:test_id', async (req, res, next) => {
 
-      let categories = [
-         {
-            "categoryID": 1,
-            "categoryName": "politics"
-         },
-         {
-            "categoryID": 2,
-            "categoryName": "breaking news"
-         },
-         {
-            "categoryID": 3,
-            "categoryName": "business"
-         },
-         {
-            "categoryID": 4,
-            "categoryName": "technology"
-         },
-         {
-            "categoryID": 5,
-            "categoryName": "health"
-         },
-         {
-            "categoryID": 6,
-            "categoryName": "travel"
-         },
-         {
-            "categoryID": 7,
-            "categoryName": "sports"
-         },
-      ];
+      // let db = await mysql.connect();
+      // let [articles] = await db.execute("SELECT * FROM articles WHERE fk_category_id = ?", req.params.test_id);
+      // db.end();
 
-      res.render('test', {
-         "title": "The News Paper - News & Lifestyle Magazine Template",
-         "categories": categories
-      });
+      // res.render('test', {
+      //    "title": "The News Paper - News & Lifestyle Magazine Template"
+      // });
+
+      res.send(req.params.test_id);
 
    });
 
