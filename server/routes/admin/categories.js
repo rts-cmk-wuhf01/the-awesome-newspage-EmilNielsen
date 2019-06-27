2e678e89daa061752bb7c3ba97e81dc6d6435117
Emil Nielsen <342703@RTS-365.dk>
2019-06-27 12:46:42 +0200

    kan nu hente en specifik kategori og autofill i formularen

--- server/routes/admin/categories.js
+++ server/routes/admin/categories.js
@@ -32,13 +32,14 @@ module.exports = (app) => {
    // adds a new category to the list
    app.post("/admin/categories", async (req, res, next) => {
 
+      
       let name = req.body.category_title;
-
+      
       let return_message = [];
       if (name == undefined || name == "") {
          return_message.push("Name is missing");
       }
-
+      
       if (return_message.length > 0) {
          console.log(messageText);
          res.render("admin/categories", {
@@ -47,14 +48,33 @@ module.exports = (app) => {
       } else {
          let db = await mysql.connect();
          let result = await db.execute(`
-   INSERT INTO categories 
-      (category_title) 
-   VALUES 
-      (?)`, [name]);
+         INSERT INTO categories 
+         (category_title) 
+         VALUES 
+         (?)`, [name]);
          db.end();
       }
+
+      let categories = await getCategories();
+
       res.render("admin/categories", {
-         "return_message": return_message.join(", ")
+         "return_message": return_message.join(", "),
+         "categories": categories
+      });
+
+   });
+
+   app.get("/admin/categories/edit/:category_id", async (req, res, next) => {
+
+      let categories = await getCategories();
+
+      let db = await mysql.connect();
+      let [selectedCategory] = await db.execute(`SELECT * FROM categories WHERE category_id = ?`, [req.params.category_id]);
+      db.end();
+
+      res.render("admin/categories", {
+         "categories": categories,
+         "selectedCategory": selectedCategory[0]
       });
 
    });
